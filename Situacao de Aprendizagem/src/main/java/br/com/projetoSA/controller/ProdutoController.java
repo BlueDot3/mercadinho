d8d5e07c9bd7788c6dc2420a7d0f0df659f934ff
lucasrluz <lucasr.luzbr@gmail.com>
2021-03-20 19:54:16 -0300

    Remoção de regras de negócio do produto controller

--- Situacao de Aprendizagem/src/main/java/br/com/projetoSA/controller/ProdutoController.java
+++ Situacao de Aprendizagem/src/main/java/br/com/projetoSA/controller/ProdutoController.java
@@ -4,16 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import br.com.projetoSA.model.Produto;
 import br.com.projetoSA.repository.ProdutoRepository;
+import br.com.projetoSA.service.ProdutoService;
 
 @Controller
 public class ProdutoController {
 
 	@Autowired
 	ProdutoRepository produtoRepository;
+
+	@Autowired
+	ProdutoService produtoService;
 	
 	@GetMapping("/produto/list")
 	public String listProduto(Model model) {
@@ -26,15 +33,32 @@ public class ProdutoController {
 		model.addAttribute("produto", produtoRepository.findById(id));
 		return "/produto/view";
 	}
-	
-	
+
+	@GetMapping("/produto/add")
+	public String addProduto() {
+		return "produto/add";
+	}
+
+	@PostMapping("/produto/save")
+	public String saveProduto(Produto produto) {
+
+		return produtoService.saveAddProduto(produto);
+	}
+
 	@GetMapping("/produto/edit")
 	public String editProduto() {
 		return "produto/edit";
 	}
 	
-	@GetMapping("/produto/add")
-	public String addProduto() {
-		return "produto/add";
+	@PutMapping("/produto/save/{id}")
+	public String saveEditProduto(@PathVariable Long id, Produto produto) {
+		
+		return produtoService.saveEditProduto(id, produto);
+	}
+	
+	@DeleteMapping("/produto/delete/{id}")
+	public String deleteProduto(@PathVariable Long id) {
+
+		return produtoService.deleteProduto(id);
 	}
 }
