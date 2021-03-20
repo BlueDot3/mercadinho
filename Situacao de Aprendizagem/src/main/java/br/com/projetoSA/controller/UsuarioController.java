338a23d639a68a6550e282aaee89d00322fa2abd
lucasrluz <lucasr.luzbr@gmail.com>
2021-03-20 12:18:43 -0300

    Remoção de regras de negócio do controller

--- Situacao de Aprendizagem/src/main/java/br/com/projetoSA/controller/UsuarioController.java
+++ Situacao de Aprendizagem/src/main/java/br/com/projetoSA/controller/UsuarioController.java
@@ -3,13 +3,16 @@ package br.com.projetoSA.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 import br.com.projetoSA.model.Usuario;
 import br.com.projetoSA.repository.UsuarioRepository;
+import br.com.projetoSA.service.UsuarioService;
 
 @Controller
 public class UsuarioController {
@@ -17,6 +20,9 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@Autowired
+	private UsuarioService usuarioService;
+
 	@GetMapping("/usuario/view")
 	public String viewUsuario(Model model, @CurrentSecurityContext(expression="authentication.name") String login) {
 
@@ -31,41 +37,30 @@ public class UsuarioController {
 		model.addAttribute("usuario", new Usuario());
 		return "usuario/add";
 	}
-	
+
 	@PostMapping("/usuario/save")
-	public String saveUsuario(Usuario usuario) {
+	public String saveAddUsuario(Usuario usuario) {
 
-		if (usuarioRepository.findByLogin(usuario.getLogin()) == null || usuarioRepository.findByCpf(usuario.getCpf()) == null) {
-
-			usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
-			
-			usuarioRepository.save(usuario);
-			
-			return "redirect:/login";
-
-		} else {
-			
-			System.out.println("Este usuário já existe!");
-
-			return "redirect:/usuario/add";
-		}	
+		return usuarioService.saveAddUsuario(usuario);	
 	}
 
-	@PostMapping("/usuario/delete")
+	@GetMapping("/usuario/edit")
+	public String editUsuario(Model model, @CurrentSecurityContext(expression="authentication.name") String login) {
+
+		model.addAttribute("usuario", usuarioRepository.findByLogin(login));
+
+		return "usuario/edit";
+	}
+	
+	@PutMapping("/usuario/save")
+	public String saveEditUsuario(Usuario usuario) {
+
+		return usuarioService.saveEditUsuario(usuario);
+	}
+	
+	@DeleteMapping("/usuario/delete")
 	public String deleteUsuario(@CurrentSecurityContext(expression="authentication.name") String login) { 
 
-		try {
-
-			usuarioRepository.deleteByLogin(login);
-
-			return "redirect:/usuario/add";
-
-		} catch (Exception e) {
-			
-			System.out.println("ERRO: " + e);
-
-			return "redirect:/usuario/view";
-
-		}	
+		return usuarioService.deleteUsuario(login);
 	}
 }
